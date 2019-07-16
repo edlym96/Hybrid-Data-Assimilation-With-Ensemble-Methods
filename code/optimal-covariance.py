b04cd5f0962e0cac8cd420a72721c002f00779d5
edlym96 <edlym96@hotmail.com>
2019-07-16 10:08:48 +0100

    added argparse to optimal-covariance

--- code/optimal-covariance.py
+++ code/optimal-covariance.py
@@ -4,7 +4,7 @@ from scipy.optimize import minimize
 import random
 import matplotlib.pyplot as plt
 import time
-
+import argparse
 from numpy.linalg import inv
 from numpy import linalg as LA
 
@@ -36,11 +36,16 @@ dimuvwTot = len(uvwTot)
 """
 
 """ Load the background state data from npz file """
-print("Loading data...")
-#uTot = np.loadtxt("../data/txt_data/background_state.txt")
-npzfile = np.load("../data/converted_data/background_state.npz")
-uTot = npzfile['u']
-print("Shape of input data is ", uTot.shape)
+
+
+def load_data(path):
+	print("Loading data...")
+	# uTot = np.loadtxt("../data/txt_data/background_state.txt")
+	npzfile = np.load(path)
+	uTot = npzfile['u']
+	print("Shape of input data is ", uTot.shape)
+	return uTot
+
 
 """Get the background errors using mean"""
 """
@@ -62,72 +67,105 @@ for j in range(n):
 
 """BELOW IS FOR ENSEMBLE METHODS"""
 """get the background errors using ensemble"""
-print("Getting background error via ensemble...")
-ensemble_size = 60
-mean = np.mean(uTot, axis = 0)
-std = np.std(uTot, axis = 0)
 
-np.random.seed(7)
+def ensemble_method(uTot, ensemble_size):
+	print("Getting background error via ensemble...")
+	mean = np.mean(uTot, axis=0)
+	std = np.std(uTot, axis=0)
+	np.random.seed(7)
+	ensemble = np.array([])
+	for i in range(mean.shape[0]):
+		#	print("Feature " + str(i))
+		samples = np.random.normal(mean[i], std[i], ensemble_size)
+		# print(mean[i],std[i],samples)
+		ensemble = np.vstack([ensemble, samples]) if ensemble.size else samples  # ensemble is of size n x Nens
+	ensemble_mean = np.expand_dims(np.mean(ensemble, axis=1), axis=1)  # Get ensemble mean
+	Xens = (1 / math.sqrt(ensemble_size - 1)) * (ensemble - ensemble_mean)  # Get ensemble errors given by formula
+	print("Ensemble is ", Xens.shape)
+	return Xens
 
-ensemble = np.array([])
-for i in range(mean.shape[0]):
-#	print("Feature " + str(i))
-	samples = np.random.normal(mean[i],std[i],ensemble_size)
-	#print(mean[i],std[i],samples)
-	ensemble = np.vstack([ensemble, samples]) if ensemble.size else samples # ensemble is of size n x Nens	
 
-ensemble_mean = np.expand_dims(np.mean(ensemble, axis = 1), axis = 1) # Get ensemble mean
-Xens = (1/math.sqrt(ensemble_size-1))*(ensemble-ensemble_mean) # Get ensemble errors given by formula
-print("Ensemble is ", Xens.shape)
 
 
 '''BELOW IS TSVD FORM OF SQUARE ROOT OF ERROR COVARIANCE'''
-print("Getting background error via TSVD")
-err = np.absolute(uTot-np.mean(uTot, axis = 0))
-
-print("err shape is ", err.shape)
-
-n = err.shape[1]
-
-#V =  np.transpose(np.reshape(err, (ntime,n)))
-V = np.transpose(err) # V has the shape n x ntime (as stated in research paper)
-
-'''
-mid = int(np.floor(ntime/2))
-U, s, W = svds(V, k=mid)
-print('first eigh!')
-st = np.sort(s)[::-1]
-s1= st[0]
-ref=math.sqrt(s1)
-print "ref", ref
-trnc=2
-while st[trnc] >= ref and trnc < mid-1 :
-        trnc=trnc+1
-        print "trnc", trnc
-        print "st[trnc]", st[trnc]
-'''
-
-trnc = 145
 
 
-print('value of trnc')
-print(trnc)
+def tsvd_method(uTot, trnc):
+	global V, Utrunc, strunc, Wtrunc, X
+	print("Getting background error via TSVD")
+	err = np.absolute(uTot - np.mean(uTot, axis=0))
+	print("err shape is ", err.shape)
+	n = err.shape[1]
+	# V =  np.transpose(np.reshape(err, (ntime,n)))
+	V = np.transpose(err)  # V has the shape n x ntime (as stated in research paper)
+	'''
+    mid = int(np.floor(ntime/2))
+    U, s, W = svds(V, k=mid)
+    print('first eigh!')
+    st = np.sort(s)[::-1]
+    s1= st[0]
+    ref=math.sqrt(s1)
+    print "ref", ref
+    trnc=2
+    while st[trnc] >= ref and trnc < mid-1 :
+            trnc=trnc+1
+            print "trnc", trnc
+            print "st[trnc]", st[trnc]
+    '''
+	Utrunc, strunc, Wtrunc = svds(V, k=trnc)
+	X = Utrunc.dot(np.diag(np.sqrt(strunc)))
+	print("V is ", V.shape)
+	print("U is ", Utrunc.shape)
+	print("s is ", strunc.shape)
+	print("WT is ", Wtrunc.shape)
+	print("U dot s is ", X.shape)
+	return X
 
 
-Utrunc, strunc, Wtrunc = svds(V, k=trnc)
-X = Utrunc.dot(np.diag(np.sqrt(strunc)))
-
-print("V is ", V.shape)
-print("U is ", Utrunc.shape)
-print("s is ", strunc.shape)
-print("WT is ", Wtrunc.shape)
-print("U dot s is ", X.shape)
-
-if not os.path.exists("../data/matrix_prec_"+str(ntime)):
-	os.mkdir("../data/matrix_prec_"+str(ntime))
-
-print("Saving preconditioned background error covariances...")
-np.savez_compressed("../data/matrix_prec_"+str(ntime)+"/matrixVprec"+str(trnc)+".npz", X)
-np.savez_compressed("../data/matrix_prec_"+str(ntime)+"/matrixVensemble" + str(ensemble_size) + ".npz", Xens)
+def save_covariance_matrices(X = None, Xens = None, ntime = ntime, trnc = 145, ensemble_size = 50):
+	if not os.path.exists("../data/matrix_prec_" + str(ntime)):
+		os.mkdir("../data/matrix_prec_" + str(ntime))
+	print("Saving preconditioned background error covariances...")
+	if X is not None:
+		np.savez_compressed("../data/matrix_prec_" + str(ntime) + "/matrixVprec" + str(trnc) + ".npz", X)
+	if Xens is not None:
+		np.savez_compressed("../data/matrix_prec_" + str(ntime) + "/matrixVensemble" + str(ensemble_size) + ".npz", Xens)
 
 
+def arg_parser():
+	parser = argparse.ArgumentParser(description='Precondition Background Error Covariance Matrices')
+	parser.add_argument('-fp',
+		'--filepath',
+		default="../data/converted_data/background_state.npz",
+		help='provide file path for background state data'
+	)
+	parser.add_argument('--trnc',
+						default=145,
+						help='provide truncation parameter for tsvd'
+						)
+	parser.add_argument('--ens_size',
+						default=50,
+						help='provide ensemble size'
+						)
+	parser.add_argument('-tsvd', action='store_true')
+	parser.add_argument('-ens', action='store_true')
+	args = parser.parse_args()
+	return args
+
+
+if __name__ == '__main__':
+	args = arg_parser()
+	filepath = args.filepath
+	trnc = args.trnc
+	ensemble_size = args.ens_size
+	uTot = load_data(filepath)
+
+	if args.tsvd and not args.ens:
+		X = tsvd_method(uTot, trnc)
+	elif args.ens and not args.tsvd:
+		Xens = ensemble_method(uTot, ensemble_size)
+	else:
+		X = tsvd_method(uTot, trnc)
+		Xens = ensemble_method(uTot, ensemble_size)
+
+	save_covariance_matrices(X, Xens, ntime, trnc, ensemble_size)
