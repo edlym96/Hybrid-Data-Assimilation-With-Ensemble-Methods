bb9900cfd7a031d84bcbbb28c0065b106eaebb29
edlym96 <edlym96@hotmail.com>
2019-07-17 01:11:59 +0100

    refactored the VarDA training

--- code/VarDA-3Dtracers+Covariance.py
+++ code/VarDA-3Dtracers+Covariance.py
@@ -14,22 +14,10 @@ from scipy.sparse.linalg import svds
 
 
 import sys
-sys.path.append('fluidity-master')
 #import vtktools
+import argparse
+sys.path.append('fluidity-master')
 
-
-#ntime = 989 # Set the number of time steps
-ntime = 989//2 # Set the number of time steps
-trnc = 145 #501 # Set the truncation parameter
-
-m = trnc
-
-#  V is the truncated form of square root of the background error covariance, B
-#V = np.loadtxt('/.../matrixV'+str(m)+'-velocity.txt', usecols=range(m))
-#V = np.load('../data/matrix_prec_' + str(ntime) + '/matrixVprec' + str(trnc) + '.npz')['arr_0']
-V = np.load('../data/matrix_prec_' + str(ntime) + '/matrixVensemble50.npz')['arr_0']
-
-print("V", V.shape)
 
 """
 # Read .vtu file
@@ -50,110 +38,128 @@ pos=ug.GetLocations()
 z=pos[:,2]
 """
 
-lam = 0.1e-60
-
-#n = len(uvwVec)
-
-# Background state matrix
-#xB = np.expand_dims(np.load('../data/converted_data/background_state.npz')['u'][10], axis = 0)
-xB = np.transpose(np.load('../data/converted_data/background_state.npz')['u']) # Transpose from 989x100040 to 100040x989
-n = xB.shape[0]
-
-print("xB", xB.shape)
-
-# Observations
-#y = np.expand_dims(np.load('../data/converted_data/observations.npz')['y'][10], axis = 0)
-y = np.transpose(np.load('../data/converted_data/observations.npz')['y'])  # Transpose from 989x100040 to 100040x989 
-print("y", y.shape)
-
-# Observation error covariance matrix
-R = lam * 0.9
-
-# Initial conditions set to a vector of ones
-#x0 = np.ones(n)
-x0 = np.ones([n,ntime])
-
-# Get inverse of V
-Vin = np.linalg.pinv(V)
-
-print("Vin", Vin.shape)
-
-"""v0 is u0 in research paper. """
-v0 = np.dot(Vin,x0)  # Take x0 from physical to reduced space by dotting with inverse of reduced V (This works because of the way the cost function is defined dx = Vdu)
-# Get transpose of V
-VT = np.transpose(V)
-
-# Background state in obervation space. H is taken as identity matrix so a direct copy can be used.
-HxB = xB.copy()
-
-# Misfit calculated by subtracting
-d = np.subtract(y,HxB) # Transpose from 989x100040 to 100040x989
-
-print("v0 ", v0.shape)
-print("d", d.shape)
-
-"""Right now, cost function uses approximate form of Background error covariance matrix, B
-	Need to change it from using B=VVT to Pf where Pf is an ensemble of error covariances.
-	Should build ensemble Pf using u from optimal-covariance.py file.
-	Find C to perform localisation
-	Change Cost function and grad
-"""
-
-# Need to structure the cost function to take in 1d input and reshape it back into matrix
-
-# Cost function J
-def J(v):
-	v = v.reshape((V.shape[1],ntime)) # need to reshape because optimize flattens input
-	#v = v.reshape((V.shape[1],1)) 
-	vT = np.transpose(v)
-	vTv = np.dot(vT,v)
-	Vv = np.dot(V,v)
-	Jmis = np.subtract(Vv,d)
-	invR = 1/R
-	JmisT = np.transpose(Jmis)
-	RJmis = JmisT.copy()
-	J1 = invR*np.dot(RJmis,Jmis)
-	Jv = (vTv + J1) / 2
-	return LA.norm(Jv,2)
+def build_DA_solution(xB_filepath, y_filepath, V_filepath, ntime = 989//2):
 
 
-# Gradient of J
-# In this case, the adjoint operator, g is taken as identity
-def gradJ(v):
-	v = v.reshape((V.shape[1],ntime))
-	#v = v.reshape((V.shape[1],1)) 
-	Vv = np.dot(V,v)
-	Jmis = np.subtract(Vv,d)
-	invR = 1/R
-	#g1 = Jmis.copy()
+	xB = np.transpose(
+		np.load(xB_filepath)['u'])  # Transpose from 989x100040 to 100040x989
+	print("xB", xB.shape)
+
+	n = xB.shape[0]
+	y = np.transpose(
+		np.load(y_filepath)['y'])  # Transpose from 989x100040 to 100040x989
+	print("y", y.shape)
+
+	# V = np.load('../data/matrix_prec_' + str(ntime) + '/matrixVprec' + str(trnc) + '.npz')['arr_0']
+	V = np.load(V_filepath)['arr_0']
+	lam = 0.1e-60
+	R = lam * 0.9
+	x0 = np.ones(n)
+
+	Vin = np.linalg.pinv(V)
+	print("Vin", Vin.shape)
+
+	v0 = np.dot(Vin,
+				x0)  # Take x0 from physical to reduced space by dotting with inverse of reduced V (This works because of the way the cost function is defined dx = Vdu)
+	print("v0 ", v0.shape)
 	VT = np.transpose(V)
-	#g2 = np.dot(VT,g1)
-	#gg2 = np.multiply(invR , g2)
-	gg2 = np.multiply(invR,np.dot(VT,Jmis)) #VT[501x100040] Jmis[989x100040]
-	ggJ = v + gg2
-	return ggJ.flatten()
 
-# Compute the minimum
-t = time.time()
+	# Background state in obervation space. H is taken as identity matrix so a direct copy can be used.
+	HxB = xB.copy()
 
-res = minimize(J, v0, method='L-BFGS-B', jac=gradJ, options={'disp': True})
+	# Misfit calculated by subtracting
+	d = np.subtract(y, HxB)
+	t = time.time()
+	for i in range(ntime):
+		di = d[:, i]
+		# Need to structure the cost function to take in 1d input and reshape it back into matrix
 
-vDA = np.array([])
-vDA = np.reshape(res.x,(V.shape[1],ntime))
+		deltaxDA = np.array([])
 
-deltaxDA = np.dot(V,vDA) # take vDA from the reduced space back to x-space
-xDA = xB + deltaxDA
+		# Cost function J
+		def J(v):
+			# v = v.reshape((V.shape[1],ntime)) # need to reshape because optimize flattens input
+			v = v.reshape((V.shape[1], 1))
+			vT = np.transpose(v)
+			vTv = np.dot(vT, v)
+			Vv = np.dot(V, v)
+			Jmis = np.subtract(Vv, di)
+			invR = 1 / R
+			JmisT = np.transpose(Jmis)
+			RJmis = JmisT.copy()
+			J1 = invR * np.dot(RJmis, Jmis)
+			Jv = (vTv + J1) / 2
+			# return LA.norm(Jv, 2)
+			return Jv
 
-elapsed = time.time() - t
-print('elapsed', elapsed, '\n')
+		# Gradient of J
+		# In this case, the adjoint operator, g is taken as identity
+		def gradJ(v):
+			# v = v.reshape((V.shape[1],ntime))
+			v = v.reshape((V.shape[1], 1))
+			Vv = np.dot(V, v)
+			Jmis = np.subtract(Vv, di)
+			invR = 1 / R
+			# g1 = Jmis.copy()
+			# g2 = np.dot(VT,g1)
+			# gg2 = np.multiply(invR , g2)
+			gg2 = np.multiply(invR, np.dot(VT, Jmis))  # VT[501x100040] Jmis[989x100040]
+			ggJ = v + gg2
+			# return ggJ.flatten()
+			return ggJ
 
-errxB = y - xB
-MSExb = LA.norm(errxB, 2)/LA.norm(y, 2)
-print('L2 norm of the background error' , MSExb , '\n')
+		res = minimize(J, v0, method='L-BFGS-B', jac=gradJ, options={'disp': True})
 
-errxDA = y - xDA
-MSExDA = LA.norm(errxDA, 2)/LA.norm(y, 2)
-print('L2 norm of the error in DA solution' , MSExDA , '\n')
+		vDA = np.reshape(res.x, (V.shape[1], 1))
+
+		deltaxDAi = np.dot(V, vDA)  # take vDA from the reduced space back to x-space
+
+		deltaxDA = np.hstack([deltaxDA,deltaxDAi]) if deltaxDA.shape else deltaxDAi
+
+	elapsed = time.time() - t
+	print('elapsed', elapsed, '\n')
+
+	xDA = xB + deltaxDA
+
+	errxB = y - xB
+	MSExb = LA.norm(errxB, 2) / LA.norm(y, 2)
+	print('L2 norm of the background error', MSExb, '\n')
+
+	errxDA = y - xDA
+	MSExDA = LA.norm(errxDA, 2) / LA.norm(y, 2)
+	print('L2 norm of the error in DA solution', MSExDA, '\n')
+
+
+def arg_parser():
+	parser = argparse.ArgumentParser(description='Calculate DA Solution')
+	parser.add_argument('-xBp',
+						'--xB_filepath',
+						default="../data/converted_data/background_state.npz",
+						help='provide file path for background state data'
+						)
+	parser.add_argument('-yp',
+						'--y_filepath',
+						default="../data/converted_data/observations.npz",
+						help='provide file path for observation data'
+						)
+	parser.add_argument('-Vp',
+						'--V_filepath',
+						default=".../data/matrix_prec_494/matrixVprec145.npz",
+						help='provide file path for observation data'
+						)
+	parser.add_argument('--ntime',
+						default=494,
+						help='number of timesteps'
+						)
+	args = parser.parse_args()
+	return args
+
+
+if __name__ == '__main__':
+	args = arg_parser()
+
+	build_DA_solution(args.xB_filepath, args.y_filepath, args.V_filepath)
+
 
 """
 ug.AddScalarField('uDA', xDA)
